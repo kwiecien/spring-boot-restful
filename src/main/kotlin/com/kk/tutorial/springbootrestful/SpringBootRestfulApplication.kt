06bb539a71a599f6673f7eeb6f9b1caf77a3bea3
kwiecien <k.d.kwiecinski@gmail.com>
2018-10-05 19:09:29 +0200

    Initial commit

---
+++ src/main/kotlin/com/kk/tutorial/springbootrestful/SpringBootRestfulApplication.kt
@@ -0,0 +1,11 @@
+package com.kk.tutorial.springbootrestful
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootRestfulApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootRestfulApplication>(*args)
+}
---
+++ src/test/kotlin/com/kk/tutorial/springbootrestful/SpringBootRestfulApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.kk.tutorial.springbootrestful
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringBootRestfulApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
