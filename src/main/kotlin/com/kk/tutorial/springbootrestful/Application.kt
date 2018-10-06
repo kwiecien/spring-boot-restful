db642f17a937daeec232d06512f257025b2d545d
kwiecien <k.d.kwiecinski@gmail.com>
2018-10-06 20:06:05 +0200

    feat: Make project runnable with bootRun task

--- src/main/kotlin/com/kk/tutorial/springbootrestful/Application.kt
+++ src/main/kotlin/com/kk/tutorial/springbootrestful/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringBootRestfulApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<SpringBootRestfulApplication>(*args)
+    runApplication<Application>(*args)
 }
--- src/main/kotlin/com/kk/tutorial/springbootrestful/GreetingController.kt
+++ src/main/kotlin/com/kk/tutorial/springbootrestful/GreetingController.kt
@@ -2,8 +2,10 @@ package com.kk.tutorial.springbootrestful
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
+@RestController
 class GreetingController {
     private val counter = AtomicLong()
 
